31d98711f200e833de69710cfd21ca2f7998afd5
David Niño Angelo Pattaguan <57347782+davidpattaguan@users.noreply.github.com>
2022-03-22 14:04:32 +0800

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID F5r4yz1snnvwLo8OJP_DGe6Q3yH-rkDUSd9ygi15fL8",
+    Authorization: "<add your API key here>",
   },
 });
